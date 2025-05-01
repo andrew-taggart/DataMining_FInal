6dba2bea149e3eff6eb362b488738996cd92b920
Andrew Taggart <156398347+andrew-taggart@users.noreply.github.com>
2025-05-01 12:50:53 -0400

    J heatmap (#5)
    * Add HeatMap.ipynb to j_heatmap_test
    
    * Add Heat_Map.py to j_heatmap_test
    
    * Update Heat_Map.py
    
    Signed-off-by: AnnaD1999 <dervishajj1@montclair.edu>
    
    ---------
    
    Signed-off-by: AnnaD1999 <dervishajj1@montclair.edu>
    Co-authored-by: AnnaD1999 <dervishajj1@montclair.edu>

---
+++ j_heatmap_test/Heat_Map.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+
+scores = []
+sentiment_labels = {"Positive": 0, "Negative": 0, "Neutral": 0}
+heatmap_data = []  #  for source-domain sentiment mapping
+
+for art in articles:
+    score = analyze_sentiment(art.content)
+    scores.append(score)
+
+    # Count sentiment categories
+    if score > 0:
+        sentiment_labels["Positive"] += 1
+    elif score < 0:
+        sentiment_labels["Negative"] += 1
+    else:
+        sentiment_labels["Neutral"] += 1
+
+    # Build heatmap source info using URL
+    if art.url:
+        source = art.url.split('/')[2]  # crude domain extraction
+        heatmap_data.append({'source': source, 'sentiment': score})
+
+
+# In[15]:
+
+
+# Heatmap of sentiment per source domain
+print("\nGenerating heatmap by news source...\n")
+
+import seaborn as sns
+import pandas as pd
+
+df = pd.DataFrame(heatmap_data)
+pivot = df.groupby('source')['sentiment'].mean().reset_index()
+pivot = pivot.pivot_table(index='source', values='sentiment')
+pivot = pivot.sort_values(by='sentiment', ascending=False)
+
+import matplotlib.pyplot as plt
+plt.figure(figsize=(10, 8))
+sns.heatmap(
+    pivot,
+    annot=True,
+    cmap='coolwarm',
+    center=0,
+    linewidths=0.5,
+    cbar_kws={'label': 'Average Sentiment Score'}
+)
+plt.title("Average Sentiment Score by News Source (from URL domain)")
+plt.ylabel("News Source Domain")
+plt.xlabel("Sentiment")
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
